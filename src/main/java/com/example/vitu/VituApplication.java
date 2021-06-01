f98f2af57e7c6ad575107aee598ed2c768692445
Victor Neves <victorneveshs@gmail.com>
2021-06-01 12:24:39 -0500

    Create a project

---
+++ src/main/java/com/example/vitu/VituApplication.java
@@ -0,0 +1,13 @@
+package com.example.vitu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VituApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VituApplication.class, args);
+	}
+
+}
